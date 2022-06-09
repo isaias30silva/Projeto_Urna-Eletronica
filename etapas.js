06c7cc58e9fc6b93c61d8029dba50cf34a6ecd40
Isaias <99995191+isaias30silva@users.noreply.github.com>
2022-06-09 15:54:02 -0300

    update

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+        {
+            numero: '38111',
+            nome: 'Silvia Maria',
+            partido: 'ABC',
+            fotos: [
+                {url: 'candidato_38111.jpg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: '77222',
+            nome: 'Pedro Henrique',
+            partido: 'DEF',
+            fotos: [
+                {url: 'candidato_77222.jpg', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '84',
+                nome: 'Ubiratan Nunes</br>Vice: Patrícia Araújo',
+                partido: 'GHI',
+                vice: 'Patrícia Araújo',
+                fotos: [
+                    {url: 'candidato84.jpg', legenda: 'Prefeito'},
+                    {url: 'candidato84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '99',
+                nome: 'Rui Dorneles</br>Vice: Ruth Gomes',
+                partido: 'JKL',
+                vice: 'Ruth Gomes',
+                fotos: [
+                    {url: 'candidato99_2.jpg', legenda: 'Prefeito'},
+                    {url: 'candidato99.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
